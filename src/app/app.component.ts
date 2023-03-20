2f3fb09a1ef5b6eacd12e465ad594140a0a09230
andreidigori <andrei@digori.com>
2023-03-20 14:09:37 +0200

    first real commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,110 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  inputForm = this.fb.nonNullable.group({
+    dShowMask: false,
+    dSize: 128,
+    dStroke: 12,
+    aShowMask: false,
+    aSize: 128,
+    aStroke: 12,
+    aOffset: 64,
+    aPercentage: 50
+  });
+
+  get showMaskOfD() {
+    return this.inputForm.controls.dShowMask.value;
+  }
+
+  get sizeOfD() {
+    return this.inputForm.controls.dSize.value;
+  }
+
+  get strokeOfD() {
+    return this.inputForm.controls.dStroke.value;
+  }
+
+  get areaOfD() {
+    return Math.pow(this.sizeOfD, 2);
+  }
+
+  get showMaskOfA() {
+    return this.inputForm.controls.aShowMask.value;
+  }
+
+  get sizeOfA() {
+    return this.inputForm.controls.aSize.value;
+  }
+
+  get heightOfA() {
+    return this.sizeOfA / Math.sqrt(2);
+  }
+
+  get maxBarWidthOfA() {
+    return this.heightOfA * 2 - this.strokeOfA * Math.sqrt(2)
+  }
+
+  get maxBarOffsetOfA() {
+    return this.sizeOfA / 2 + this.strokeOfA * (0.5 - 1 / Math.sqrt(2));
+  }
+
+  get strokeOfA() {
+    return this.inputForm.controls.aStroke.value;
+  }
+
+  get offsetOfA() {
+    return this.sizeOfD * this.inputForm.controls.aOffset.value / 100;
+  }
+
+  get percentageOfA() {
+    return this.inputForm.controls.aPercentage.value / 100;
+  }
+
+  get areaOfA() {
+    return Math.pow(this.sizeOfA, 2) / 2;
+  }
+
+  get sizeOfCanvas() {
+    return this.offsetOfA + this.sizeOfA;
+  }
+
+  get centerOfMass() {
+    return (this.sizeOfD / 2 * this.areaOfD + (this.offsetOfA + this.sizeOfA / 3) * this.areaOfA) / (this.areaOfD + this.areaOfA);
+  }
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.inputForm.controls.dSize.valueChanges.subscribe(value => {
+      this.inputForm.controls.aSize.setValue(value);
+    });
+    this.inputForm.controls.dStroke.valueChanges.subscribe(value => {
+      this.inputForm.controls.aStroke.setValue(value);
+    });
+    this.loadInput();
+    this.inputForm.valueChanges.subscribe(input => this.saveInput(input));
+  }
+
+  private loadInput() {
+    const value = window.localStorage.getItem('dasimpleLogo') || '';
+    try {
+      const input = JSON.parse(value);
+      this.inputForm.patchValue(input);
+    } catch (e) {
+
+    }
+  }
+
+  private saveInput(input: any) {
+    const value = JSON.stringify(input);
+    window.localStorage.setItem('dasimpleLogo', value);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
